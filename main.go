450c4a54b5cef1b9d6c35c6df18d56ccdf2a478a
William Chou <williamchou@kklab.com>
2023-01-19 12:49:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,30 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/png"
 	"log"
 	"os"
 
 	"golang.org/x/image/draw"
+	"golang.org/x/image/math/f64"
 )
 
 func main() {
-	input, _ := os.Open("example.png")
-	defer input.Close()
-
-	src, err := png.Decode(input)
+	in, err := os.Open("in.png")
 	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	output, _ := os.Create("example_resized.png")
-	defer output.Close()
-
-	dst := image.NewRGBA(image.Rect(0, 0, src.Bounds().Max.X/2, src.Bounds().Max.Y/2))
-	draw.CatmullRom.Scale(dst, dst.Rect, src, src.Bounds(), draw.Over, nil)
-
-	if err = png.Encode(output, dst); err != nil {
 		log.Fatal(err)
 	}
+	defer in.Close()
+	src, _, err := image.Decode(in)
+	if err != nil {
+		log.Fatal(err)
+	}
+	transformed := image.NewRGBA(image.Rect(50, 50, 150, 150))
+	draw.Copy(transformed, image.Point{0, 0}, src, src.Bounds(), draw.Over, nil)
+	draw.ApproxBiLinear.Transform(transformed, f64.Aff3{}, src, src.Bounds(), draw.Over, nil)
+	scaled := image.NewRGBA(image.Rect(0, 0, 120, 120))
+	draw.ApproxBiLinear.Scale(scaled, scaled.Rect, transformed, transformed.Bounds(), draw.Over, nil)
+	out, err := os.Create("out.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	err = png.Encode(out, scaled)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("image has bounds %v.\n", scaled.Bounds())
 }
