a5223836577c1cb652cd014b02938839d0617d3e
William Chou <williamchou@kklab.com>
2023-01-30 12:17:42 +0800

    Add cli vars

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/png"
@@ -11,8 +12,33 @@ import (
 	"golang.org/x/image/math/f64"
 )
 
+var (
+	config Config
+)
+
+type Config struct {
+	In     string
+	Out    string
+	X0     int
+	Y0     int
+	Width  int
+	Height int
+	Size   int
+}
+
+func init() {
+	flag.StringVar(&config.In, "i", "in.png", "input image")
+	flag.StringVar(&config.Out, "o", "out.png", "output image")
+	flag.IntVar(&config.X0, "x", 0, "x of the image")
+	flag.IntVar(&config.Y0, "y", 0, "y of the image")
+	flag.IntVar(&config.Width, "w", 0, "width of the image")
+	flag.IntVar(&config.Height, "h", 0, "height of the image")
+	flag.IntVar(&config.Size, "s", 480, "size of the image")
+	flag.Parse()
+}
+
 func main() {
-	in, err := os.Open("in.png")
+	in, err := os.Open(config.In)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,12 +47,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	transformed := image.NewRGBA(image.Rect(50, 50, 150, 150))
+	width := config.Width
+	if width == 0 {
+		width = src.Bounds().Max.X
+	}
+	height := config.Height
+	if height == 0 {
+		height = src.Bounds().Max.Y
+	}
+	transformed := image.NewRGBA(image.Rect(config.X0, config.Y0, config.X0+width, config.Y0+height))
 	draw.Copy(transformed, image.Point{0, 0}, src, src.Bounds(), draw.Over, nil)
-	draw.ApproxBiLinear.Transform(transformed, f64.Aff3{}, src, src.Bounds(), draw.Over, nil)
-	scaled := image.NewRGBA(image.Rect(0, 0, 120, 120))
-	draw.ApproxBiLinear.Scale(scaled, scaled.Rect, transformed, transformed.Bounds(), draw.Over, nil)
-	out, err := os.Create("out.png")
+	draw.BiLinear.Transform(transformed, f64.Aff3{}, src, src.Bounds(), draw.Over, nil)
+	scaled := image.NewRGBA(image.Rect(0, 0, config.Size, config.Size))
+	draw.BiLinear.Scale(scaled, scaled.Rect, transformed, transformed.Bounds(), draw.Over, nil)
+	out, err := os.Create(config.Out)
 	if err != nil {
 		log.Fatal(err)
 	}
